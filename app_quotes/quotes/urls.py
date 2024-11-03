1a00f3b472054ca0283e80aaf705835c43f72f20
AndreShiZap <andreshizap@gmail.com>
2024-11-03 11:45:53 +0200

    Add files via upload

--- app_quotes/quotes/urls.py
+++ app_quotes/quotes/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path("add-quote/", views.add_quote, name="add_quote"),
     path('tag/<str:tag_name>/', views.quotes_by_tag, name='quotes_by_tag'),
     path('tag/<str:tag_name>/<int:page>/', views.quotes_by_tag, name='quotes_by_tag'),
-    path("quotes/<author_id>/", views.author_detail, name="author_detail")
+    path("quotes/<author_id>/", views.author_detail, name="author_detail"),
+    path("run_spider/", views.run_spider, name="run_spider"),
+    path("check_spider_status/", views.check_spider_status, name="check_spider_status")
 ]
